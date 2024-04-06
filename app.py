4eaa935dcd458a1fe5420da46a9446d537386cce
Sergey Zinkin <zinkin.s@yandex.ru>
2024-04-06 20:20:22 +0300

    Add function merge_datasets

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as pt
+import numpy as np
 
 def load_dataset(path):
     df = pd.read_csv(path)
     return df
 
+def merge_datasets(df1, df2):
+    df = pd.concat([df1, df2], ignore_index=True)
+    return df
+
+
+
+
 st.title('Анализ заработных плат в Российской Федерации')
 
-path = 'data//sheet_1.csv'
-data = load_dataset(path)
+path_1 = 'data//sheet_1.csv'
+sheet_1 = load_dataset(path_1)
+
+path_2 = 'data//sheet_2.csv'
+sheet_2 = load_dataset(path_2)
+sheet_2 = sheet_2.rename(columns={'Всего': 'Всего по  экономике'})
+
+data = merge_datasets(sheet_1[['Год', 'Всего по  экономике']], sheet_2[['Год', 'Всего по  экономике']])
 st.dataframe(data)
